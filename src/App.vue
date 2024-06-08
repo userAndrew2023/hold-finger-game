dc517f48ec9501ce66bbea9429364470a0960c85
andrey <andrewwf@yandex.ru>
2024-06-08 15:29:34 +0300

    master

--- src/App.vue
+++ src/App.vue
@@ -114,14 +114,6 @@ export default {
     printAgent() {
       return navigator.userAgent;
     }
-  },
-  mounted() {
-    const overflow = 100
-document.body.style.overflowY = 'hidden'
-document.body.style.marginTop = `${overflow}px`
-document.body.style.height = window.innerHeight + overflow + "px"
-document.body.style.paddingBottom = `${overflow}px`
-window.scrollTo(0, overflow)
   }
 };
 </script>
