0460329df4a46544637727e0b4f655d23b1f8de4
andrew777f <asdfdjfdnmcdk@yandex.by>
2024-06-15 16:28:46 +0300

    master

--- src/views/FriendsView.vue
+++ src/views/FriendsView.vue
@@ -1,8 +1,34 @@
 <template>
     <div>
-        <div class="invite-label">40% from each fren</div>
-        <div class="copy-link" @click="sendLink()">Invite a fren (+1000)</div>
+        <div class="invite-label">40% from each fren!</div>
+        <div class="copy-link" @click="sendLink()">Invite a fren (+1000 KTC)</div>
         <div class="hr"></div>
+        <div class="frens-wrapper">
+            <div class="frens-label">Your frens</div>
+            <div class="frens-list">
+                <div class="fren-item" style="display: flex" @click="window.location.href = 'https://t.me/V_Zelenskiy_official/5182'">
+                    <div class="fren-image-wrapper">
+                        <img class="fren-image" src="https://www.film.ru/sites/default/files/people/_tmdb/mfmyYf8w80ESsAoOF8IN9DSjQtz.jpg">
+                    </div>
+                    <div class="fren-info">
+                        <div>
+                            <div class="fren-name-wrapper">
+                                <div class="fren-name">Joooe Biden</div>
+                                <div class="fren-earn">
+                                    <img src="@/assets/image.png" class="task-coin-logo">
+                                    <div class="money">5000</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <span class="material-symbols-outlined">
+                                arrow_forward_ios
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
         <ToastNotification ref="toastNotification"/>
     </div>
 </template>
@@ -26,9 +52,10 @@ export default {
 .invite-label {
     padding-top: 20px;
     font-size: 24px;
+    font-weight: 700;
 }
 .copy-link {
-    margin-top: 30px;
+    margin-top: 20px;
     padding: 10px;
     padding-left: 30px;
     border-radius: 7px;
@@ -40,8 +67,54 @@ export default {
 }
 .hr {
     border-bottom: 1px solid #4d4d4d;
-    margin-top: 20px;
+    margin-top: 25px;
     margin-right: 20px;
     margin-left: 20px;
+    margin-bottom: 25px;
+}
+.frens-wrapper {
+    margin-right: 20px;
+    margin-left: 20px;
+}
+.frens-label {
+    font-weight: 500;
+    font-size: 20px;
+    text-align: left;
+}
+.frens-list {
+    margin-top: 20px;
+}
+.fren-image-wrapper {
+    margin-right: 10px;
+    width: 45px;
+    height: 45px;
+}
+.fren-image {
+    width: 45px;
+    height: 45px;
+    border-radius: 50%;
+}
+.fren-info {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: 1px solid #4d4d4d;
+    width: 100%;
+}
+.fren-name-wrapper {
+    text-align: left;
+    padding-bottom: 20px;
+}
+.fren-earn {
+    display: flex;
+    margin-top: 5px;
+}
+.money {
+    margin-left: 5px;
+    font-size: 14px;
+}
+.task-coin-logo {
+    border-radius: 50%;
+    width: 20px;
+    height: 20px;
 }
 </style>
