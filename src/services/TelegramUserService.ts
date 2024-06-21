ea18f9c6c428d520d18b360f19d28f893bfcd858
andrey <andrewwf@yandex.ru>
2024-06-21 20:36:31 +0300

    master

--- src/services/TelegramUserService.ts
+++ src/services/TelegramUserService.ts
@@ -3,23 +3,23 @@ import { TelegramUser } from '../models/TelegramUser';
 
 const telegramUserService =  {
   async getUsers(): Promise<TelegramUser[]> {
-    const response = await apiClient.get<TelegramUser[]>('/telegram-users');
+    const response = await apiClient.get<TelegramUser[]>('/telegram-users/');
     return response.data;
   },
   async getUser(userId: number): Promise<TelegramUser> {
-    const response = await apiClient.get<TelegramUser>(`/telegram-users/${userId}`);
+    const response = await apiClient.get<TelegramUser>(`/telegram-users/${userId}/`);
     return response.data;
   },
   async getUserByTelegramId(telegramId: string): Promise<TelegramUser> {
-    const response = await apiClient.get<TelegramUser>(`/telegram-users/${telegramId}?type=telegram`);
+    const response = await apiClient.get<TelegramUser>(`/telegram-users/${telegramId}?type=telegram/`);
     return response.data;
   },
   async createUser(user: TelegramUser): Promise<TelegramUser> {
-    const response = await apiClient.post<TelegramUser>('/telegram-users', user);
+    const response = await apiClient.post<TelegramUser>('/telegram-users/', user);
     return response.data;
   },
   async updateUser(user: any): Promise<TelegramUser> {
-    const response = await apiClient.put<TelegramUser>(`/telegram-users/${user.id}`, user);
+    const response = await apiClient.put<TelegramUser>(`/telegram-users/${user.id}/`, user);
     return response.data;
   }
 };
