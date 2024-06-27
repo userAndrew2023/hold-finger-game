a24d407d87ff006cdf2b5ad8aa52dc2ebefcd086
andrew777f <asdfdjfdnmcdk@yandex.by>
2024-06-27 13:24:31 +0300

    master

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://147.45.185.114:8000/api/',
+  baseURL: 'localhost:8000/api/',
   headers: {
     'Content-Type': 'application/json',
   },
